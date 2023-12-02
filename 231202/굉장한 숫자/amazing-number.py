94c1b5c5e3d7efd34d2d4b70d01d7b5f77cda7fb
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-02 19:18:17 +0900

    Title: 굉장한 숫자, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 231202/굉장한 숫자/amazing-number.py
+++ 231202/굉장한 숫자/amazing-number.py
@@ -1,8 +1,6 @@
-n = int(input)
+n = int(input())
 
-if n%2 != 0 and n%3 == 0:
-    print('true')
-elif n%2 == 0 and n%5 == 0:
+if (n%2 != 0 and n%3 == 0 or (n%2 == 0 and n%5 == 0)):
     print('true')
 else:
     print('false')
