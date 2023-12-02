a5db1ba617cbcaff58ff89b9bb2e36769c6ff08c
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-02 20:46:03 +0900

    Title: 높은 수에서 낮은 수까지, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231202/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 231202/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -3,6 +3,8 @@ a, b = map(int, input().split())
 if a > b:
     for i in range(a, b, -1):
         print(i, end=' ')
-else:
+elif b > a:
     for i in range(b, a, -1):
         print(i, end=' ')
+else:
+    print(a)
