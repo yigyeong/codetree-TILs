2b0e3a051a2a995da12caa7a9633bf3866188b70
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-03 17:10:43 +0900

    Title: 조건에 부합하는 수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 231203/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -3,8 +3,8 @@ a = int(input())
 for i in range(1, a+1):
     if i % 2 == 0 and i % 4 != 0:
         continue
-    if (i % 8) % 2 == 0:
+    if (i // 8) % 2 == 0:
         continue
-    if (i % 7) < 4:
+    if i <= 7 or (i % 7) < 4:
         continue
     print(i, end = ' ')
