fe56c376abc7323e40b8a97c616aa704f1b909d7
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-03 15:42:36 +0900

    Title: 369 게임, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231203/369 게임/369-game.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 3 == 0:
+        print(0, end=' ')
+    elif i > 10 and (i//10 == 3 or i//10 == 6 or i//10 == 9 or i%10 == 3 or i%10 == 6 or i % 10== 9):
+        print(0, end=' ')
+    else:
+        print(i, end=' ')
