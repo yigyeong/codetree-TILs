50d1522659efc4254b30c9901c7b3e5673d94669
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-06 17:51:30 +0900

    Title: ab 사이에 없는 c, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231206/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+satisfied = True
+
+for i in range(a, b+1):
+    if i % c == 0:
+        satisfied = False
+        break
+
+if satisfied == False:
+    print('YES')
+else:
+    print('NO')
