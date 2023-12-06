f83689fd25359c927075074b455d791f18b5b163
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-06 17:06:49 +0900

    Title: 3n + 1 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231206/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,15 @@
+n = int(input())
+cnt = 0
+
+while True:
+    if n % 2 == 0:
+        n = n / 2
+        cnt += 1
+    elif :
+        n = 3 * n + 1
+        cnt += 1
+    if n == 1:
+        cnt += 1
+        break
+
+print(cnt)
