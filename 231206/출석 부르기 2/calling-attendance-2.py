2f96e78959821a004faa176a64671b3a7a0737ae
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-06 16:54:45 +0900

    Title: 출석 부르기 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231206/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True:
+    x = int(input())
+    
+    if x == 1 :
+        print('John')
+    elif x == 2:
+        print('Tom')
+    elif x== 3:
+        print('Paul')
+    elif x== 4:
+        print('Sam')
+    else:
+        print('Vacancy')
+        break
