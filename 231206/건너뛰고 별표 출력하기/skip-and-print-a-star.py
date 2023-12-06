13f1b67e9403bd4073a25372fcf8b9196ddc302b
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-06 18:36:52 +0900

    Title: 건너뛰고 별표 출력하기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 231206/건너뛰고 별표 출력하기/skip-and-print-a-star.py
+++ 231206/건너뛰고 별표 출력하기/skip-and-print-a-star.py
@@ -1,12 +1,12 @@
 n = int(input())
 cnt = 1
 
-for i in range(2*n+1):
+for i in range(2*n-1):
     for j in range(cnt):
         print('*', end='')
     print('\n')
 
-    if cnt < n-1:
+    if i < n-1:
         cnt += 1
     else:
         cnt -= 1
