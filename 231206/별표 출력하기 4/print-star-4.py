08086c3f4b23dd9302be8595ffbb894c3cf554fe
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-06 18:33:00 +0900

    Title: 별표 출력하기 4, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231206/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = n
+
+for i in range(2* n -1):
+    for j in range(cnt):
+        print('*', end = ' ')
+    print()
+
+    if i < n-1:
+        cnt -= 1
+    else:
+        cnt += 1
