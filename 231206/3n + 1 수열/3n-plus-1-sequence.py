4372ef30ebaad55eb17e703102a06a3fd608a7db
yigyeong <99374736+yigyeong@users.noreply.github.com>
2023-12-06 17:07:16 +0900

    Title: 3n + 1 수열, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231206/3n + 1 수열/3n-plus-1-sequence.py
+++ 231206/3n + 1 수열/3n-plus-1-sequence.py
@@ -5,11 +5,10 @@ while True:
     if n % 2 == 0:
         n = n / 2
         cnt += 1
-    elif :
+    else:
         n = 3 * n + 1
         cnt += 1
     if n == 1:
-        cnt += 1
         break
 
 print(cnt)
