4b8485b1ab86e8538665709f1c7cb245d731df99
yigyeong <99374736+yigyeong@users.noreply.github.com>
2024-01-13 17:30:03 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240113/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n):
+    if arr[i] == 2:
+        cnt += 1
+    
+    if cnt == 3:
+        idx = i + 1
+        break
+    
+print(idx)
