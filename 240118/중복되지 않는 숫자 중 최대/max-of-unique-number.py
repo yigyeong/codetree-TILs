d44138276eaa5cd8e60b5cb53e9066d60659423d
yigyeong <99374736+yigyeong@users.noreply.github.com>
2024-01-18 15:17:58 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240118/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+m = max(arr)
+cnt = 0
+for elem in arr:
+    if m == elem:
+        cnt+= 1
+
+if cnt == 1:
+    print(m)
+else:
+    print(-1)
